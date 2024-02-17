747cde1cdece0655f36262597226eb2ad082117e
michaelboegner <cmikeb@gmail.com>
2024-02-17 14:32:52 -0500

    classes for points and lines initiated. Able to draw simple lines

--- maze.py
+++ maze.py
@@ -19,10 +19,41 @@ class Window:
             self.redraw()
     def close(self):
         self.running = False
+    def draw_line(self, line, fillcolor):
+        line.draw(self.canvas, fillcolor)
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+class Line:
+    def __init__(self, point_1, point_2) -> None:
+        self.point_1 = point_1
+        self.point_2 = point_2
+    def draw(self, canvas, fill_color):
+        self.fill_color = fill_color
+        canvas.create_line(
+            self.point_1.x, 
+            self.point_1.y, 
+            self.point_2.x, 
+            self.point_2.y, 
+            fill=fill_color, 
+            width=2
+        )
+        canvas.pack()
+    
+
 
 def main():
     win = Window(800, 600)
-    print("THIS IS WIN", win)
+    point_1 = Point(10,10)
+    point_2 = Point(20,20)
+    line = Line(point_1, point_2)
+    point_3 = Point(10,10)
+    point_4 = Point(200,570)
+    line2 = Line(point_3, point_4)
+    win.draw_line(line,"blue")
+    win.draw_line(line2,"orange")
     win.wait_for_close()
 
 if __name__ == "__main__":
