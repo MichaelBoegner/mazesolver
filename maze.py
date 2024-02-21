7dded758d5edbd1deb9b317ebc6138b31aa41fe5
michaelboegner <cmikeb@gmail.com>
2024-02-21 18:19:36 -0500

    Cell class implemented and able to draw a single cell

--- maze.py
+++ maze.py
@@ -6,7 +6,7 @@ class Window:
         self.height = height
         self.root = Tk()
         self.root.title = "Title"
-        self.canvas = Canvas()
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
         self.canvas.pack()
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
@@ -19,8 +19,8 @@ class Window:
             self.redraw()
     def close(self):
         self.running = False
-    def draw_line(self, line, fillcolor):
-        line.draw(self.canvas, fillcolor)
+    def draw_line(self, line):
+        line.draw(self.canvas)
 
 class Point:
     def __init__(self, x, y) -> None:
@@ -30,8 +30,7 @@ class Line:
     def __init__(self, point_1, point_2) -> None:
         self.point_1 = point_1
         self.point_2 = point_2
-    def draw(self, canvas, fill_color):
-        self.fill_color = fill_color
+    def draw(self, canvas, fill_color="black"):
         canvas.create_line(
             self.point_1.x, 
             self.point_1.y, 
@@ -44,17 +43,46 @@ class Line:
     
 
 
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = False
+        self.has_right_wall = False
+        self.has_top_wall = False
+        self.has_bottom_wall = False
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._window = window
+
+    def draw_line(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._window.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._window.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._window.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._window.draw_line(line)
+            
+
 def main():
-    win = Window(800, 600)
-    point_1 = Point(10,10)
-    point_2 = Point(20,20)
-    line = Line(point_1, point_2)
-    point_3 = Point(10,10)
-    point_4 = Point(200,570)
-    line2 = Line(point_3, point_4)
-    win.draw_line(line,"blue")
-    win.draw_line(line2,"orange")
-    win.wait_for_close()
+    window = Window(800, 800)
+    cell = Cell(window)
+    cell.has_left_wall = True
+    cell.has_top_wall = True
+    cell.has_right_wall = True
+    cell.has_bottom_wall = True
+    cell.draw_line(100,100,200,200)
+    window.wait_for_close()
 
 if __name__ == "__main__":
     main()
