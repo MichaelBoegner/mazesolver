53880b62112baccad5bfa9014686cee73a6ba18e
michaelboegner <cmikeb@gmail.com>
2024-04-12 19:52:55 -0400

    began work on _break_entrance_and_exixit but only able to add entrance or exit. Something is redrawing the walls back in when doing both

--- maze.py
+++ maze.py
@@ -86,8 +86,7 @@ class Maze:
             x1,
             y1,
             num_rows,
-            num_cols,
-            cell_size_x,
+            num_cols,            cell_size_x,
             cell_size_y,
             window=None) -> None:
         self.x1 = x1
@@ -111,6 +110,7 @@ class Maze:
             for j in range(self.num_rows):
                 self._draw_cell(i, j)
 
+
     def _draw_cell(self, i, j):
         if self.win is None:
             return
@@ -118,14 +118,27 @@ class Maze:
         y1 = self.y1 + j * self.cell_size_y
         x2 = x1 + self.cell_size_x
         y2 = y1 + self.cell_size_y
-        self.cells[i][j].draw_line(x1, y1, x2, y2)
-        self._animate()
+        if (i == self.num_cols-1 and j == self.num_rows-1):
+            self._break_entrance_and_exit(x1, y1, x2, y2)
+        elif (i == 0 and j == 0):
+            self._break_entrance_and_exit(x1, y1, x2, y2)
+        else:
+            self.cells[i][j].draw_line(x1, y1, x2, y2)
+            self._animate()
 
     def _animate(self):
         if self.win is None:
             return
         self.win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self, x1, y1, x2, y2):
+        self.cells[0][0].has_top_wall = False
+        self.cells[0][0].draw_line(x1, y1, x2, y2)
+        self._animate()
+        self.cells[self.num_cols-1][self.num_rows-1].has_bottom_wall = False
+        self.cells[self.num_cols-1][self.num_rows-1].draw_line(x1, y1, x2, y2)
+        self._animate()
         
 
 def main():
